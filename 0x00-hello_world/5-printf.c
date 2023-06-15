b05a0c48517eabb774cfe61d9f1448774f79fa9a
opeyemi olaniyan <you@opeyemiolaniyan628@gmail.com>
2023-06-15 14:07:01 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,9 +3,8 @@
  * main - A printf program
  * Return: 0
  */
-
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n")
-		return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
 }
