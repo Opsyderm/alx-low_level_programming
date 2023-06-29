a42022a46ed624885cda54952170e7439895653e
opeyemi olaniyan <you@opeyemiolaniyan628@gmail.com>
2023-06-29 09:31:51 -0700

    0x46

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+
+/**
+ * _strncpy - copies at most an imputed number
+ * of bytes from string src into dest
+ * @dest: The buffer storing the string copy.
+ * @src: The source string.
+ * @n: the maximum number of bytes to be copied from src.
+ * Return: a ponter to the resulting string dest.
+ *
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+
+{
+
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+	src_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+	dest[index] = src[index];
+
+	for (index = src_len; index < n; index++)
+	dest[index] = '\0';
+
+	return (dest);
+
+}
