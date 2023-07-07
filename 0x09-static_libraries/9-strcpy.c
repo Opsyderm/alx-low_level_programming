11f7a437bae351f4e990f02c8379aac380f9426a
opeyemi olaniyan <you@opeyemiolaniyan628@gmail.com>
2023-07-07 09:41:22 -0700

    C - Static libraries

---
+++ 0x09-static_libraries/9-strcpy.c
@@ -0,0 +1,44 @@
+#include "main.h"
+		
+/**
+ * char *_strcpy - this is a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
+ 
+ 
+============================
+
+
+ 
+100-atoi.c
+
+
+#include "main.h"
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
